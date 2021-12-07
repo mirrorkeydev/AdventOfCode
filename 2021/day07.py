56f95f8ec9fccce011de8173514d127bb86fb844
mirrorkeydev <mirrorkeydev@gmail.com>
2021-12-06 21:13:21 -0800

    Clean up a bit

--- 2021/day07.py
+++ 2021/day07.py
@@ -1,22 +1,18 @@
-import re
-import numpy as np
-from collections import defaultdict
-
 with open("input.txt") as f:
   arr = list((int(x.strip()) for x in f.readlines()[0].split(",")))
 
-def get_cheapest_fuel(start, roi):
-  cheapest_fuel = start
+def get_cheapest_fuel(delta):
+  cheapest_fuel = 1000000000
   for i in range(max(arr)):
     cur_fuel = 0
     for crab in arr:
-      cur_fuel += roi(crab, i)
+      cur_fuel += delta(crab, i)
     if cur_fuel < cheapest_fuel:
       cheapest_fuel = cur_fuel
   return cheapest_fuel
 
 # Part 1
-print(get_cheapest_fuel(1000000, lambda c, i: abs(c - i)))
+print(get_cheapest_fuel(lambda c, i: abs(c - i)))
   
 # Part 2
-print(get_cheapest_fuel(1000000000, lambda c, i: int((abs(c - i)**2 + abs(c - i))/2)))
+print(get_cheapest_fuel(lambda c, i: int((abs(c - i)**2 + abs(c - i))/2)))
